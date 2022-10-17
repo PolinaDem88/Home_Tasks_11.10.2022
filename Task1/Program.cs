30cfaeda578ed4a4541c3a04532f142ce3e5974a
Polina <Polina_piter88@mail.ru>
2022-10-17 11:35:21 +0300

    1 Задача

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿ /*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16*/
+
+void GetProd()
+{
+    Console.WriteLine("Введите число A:");
+    int Anum = int.Parse(Console.ReadLine());
+    Console.WriteLine("Введите число B:");
+    int Bnum = int.Parse(Console.ReadLine());
+    int prod = 1;
+    for (int i = 1; i <= Bnum; i++)
+{
+    prod = prod * Anum;
+}
+Console.WriteLine($"{Anum} в степени {Bnum} = {prod}");
+}
+GetProd();
